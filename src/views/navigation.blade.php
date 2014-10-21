1f3c36ae3eea83815d9b9decca0f52bbbab921a2
Matteo Rosati <mrosati@h-art.it>
2014-10-21 10:44:26 +0200

    update navigation title

--- src/views/navigation.blade.php
+++ src/views/navigation.blade.php
@@ -6,7 +6,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">Administration</a>
+        <a class="navbar-brand" href="#">{{ Config::get('laradmin.title', 'Administration') }}</a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
