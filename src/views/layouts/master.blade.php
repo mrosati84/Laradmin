f6ae14e478bf810165b198c91071dcf7376d1e58
Matteo Rosati <mrosati@h-art.it>
2014-10-21 10:38:18 +0200

    move assets from cdn to local

--- src/views/layouts/master.blade.php
+++ src/views/layouts/master.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Crud</title>
+    <title>{{ Config::get('laradmin.title', 'Administration') }}</title>
 
     <!-- stylesheets -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+    <link rel="stylesheet" href="{{ URL::asset('packages/mrosati84/laradmin/css/vendor/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('packages/mrosati84/laradmin/css/vendor/bootstrap-theme.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('packages/mrosati84/laradmin/css/vendor/jquery-ui.css') }}">
     <style>
         .right-floated {
             float: right;
@@ -25,17 +25,12 @@
 </div>
 
 <!-- javascripts -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.4.5/jquery-ui-timepicker-addon.min.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<script>
-    $('input.datetime').datetimepicker({
-        'dateFormat': 'yy-mm-dd',
-        'timeFormat': 'HH:mm:ss'
-    });
-</script>
+<script src="{{ URL::asset('packages/mrosati84/laradmin/js/vendor/jquery.min.js') }}"></script>
+<script src="{{ URL::asset('packages/mrosati84/laradmin/js/vendor/jquery-ui.min.js') }}"></script>
+<script src="{{ URL::asset('packages/mrosati84/laradmin/js/vendor/jquery-ui-timepicker-addon.min.js') }}"></script>
+<script src="{{ URL::asset('packages/mrosati84/laradmin/js/vendor/angular.min.js') }}"></script>
+<script src="{{ URL::asset('packages/mrosati84/laradmin/js/vendor/bootstrap.min.js') }}"></script>
+<script src="{{ URL::asset('packages/mrosati84/laradmin/js/main.js') }}"></script>
 
 </body>
 </html>
