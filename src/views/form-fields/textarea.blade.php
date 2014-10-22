36ab4ad63d737b139e10ce5ea282189325e29d1f
Matteo Rosati <mrosati@h-art.it>
2014-10-22 18:00:48 +0200

    error message printing for string and textarea input fields

--- src/views/form-fields/textarea.blade.php
+++ src/views/form-fields/textarea.blade.php
@@ -1,3 +1,6 @@
 <div class="{{ ($errors->first($fieldName)) ? 'has-error' : '' }}">
     {{ Form::textarea($fieldName, $fieldValue, array('class' => 'form-control')) }}
+    @if($errors->first($fieldName))
+        <small>{{ $errors->first($fieldName) }}</small>
+    @endif
 </div>
