76c2c36cbeefac51cd1db818abce81638741b3cd
Matteo Rosati <mrosati@h-art.it>
2014-10-22 15:19:32 +0200

    print validation error classes

--- src/views/form-fields/string.blade.php
+++ src/views/form-fields/string.blade.php
@@ -1,1 +1,3 @@
-{{ Form::text($fieldName, $fieldValue, array('class' => 'form-control')) }}
+<div class="{{ ($errors->first($fieldName)) ? 'has-error' : '' }}">
+    {{ Form::text($fieldName, $fieldValue, array('class' => 'form-control')) }}
+</div>
--- src/views/form-fields/textarea.blade.php
+++ src/views/form-fields/textarea.blade.php
@@ -1,1 +1,3 @@
-{{ Form::textarea($fieldName, $fieldValue, array('class' => 'form-control')) }}
+<div class="{{ ($errors->first($fieldName)) ? 'has-error' : '' }}">
+    {{ Form::textarea($fieldName, $fieldValue, array('class' => 'form-control')) }}
+</div>
