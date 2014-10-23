5e09c1933ad663c259b937a9080dbda4984cef76
Matteo Rosati <mrosati@h-art.it>
2014-10-23 12:47:34 +0200

    fix file formatting

--- src/views/forms/create.blade.php
+++ src/views/forms/create.blade.php
@@ -1,8 +1,13 @@
-{{ Form::open(array('route' => $formRoute, 'method' => 'POST', 'class' => 'form-horizontal')) }}
+{{ Form::open(array(
+    'method' => 'POST',
+    'route' => $formRoute,
+    'class' => 'form-horizontal')) }}
     <!-- loop fields -->
     @foreach($fields as $fieldName => $fieldProperties)
         <div class="form-group">
-            {{ Form::label(ucfirst($fieldProperties['label']), ucfirst($fieldProperties['label']), array('class' => 'col-sm-2 control-label')) }}
+            {{ Form::label(ucfirst($fieldProperties['label']),
+                ucfirst($fieldProperties['label']),
+                array('class' => 'col-sm-2 control-label')) }}
             <div class="col-sm-10">
                 {{ $renderer('create', $className, $fieldName) }}
             </div>
