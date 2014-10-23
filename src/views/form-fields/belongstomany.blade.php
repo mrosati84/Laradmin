6898fe7e0114fae19c4fa642a4574e3d8e300924
Matteo Rosati <rosati.matteo@gmail.com>
2014-10-23 22:37:11 +0200

    add validation errors output

--- src/views/form-fields/belongstomany.blade.php
+++ src/views/form-fields/belongstomany.blade.php
@@ -1,4 +1,4 @@
-<div class="row">
+<div class="row {{ ($errors->first($fieldName)) ? 'has-error' : '' }}">
     <div class="col-sm-11">
         <!-- needed for empty list transmission -->
         <input type="hidden" name="{{ $fieldName }}" value="">
@@ -8,6 +8,9 @@
             ($fieldValue) ? $fieldValue->lists('id') : null,
             array('multiple', 'class' => 'form-control')
         )}}
+        @if($errors->first($fieldName))
+            <small>{{ $errors->first($fieldName) }}</small>
+        @endif
     </div>
     <div class="col-sm-1">
         <a class="btn btn-primary right-floated" href="#">
