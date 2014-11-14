7485c78b9443b0f814ad6c33c650d0d021d7a15a
Matteo Rosati <rosati.matteo@gmail.com>
2014-11-14 20:03:32 +0100

    add reset button to filters

--- src/views/forms/filters.blade.php
+++ src/views/forms/filters.blade.php
@@ -7,6 +7,11 @@
                 {{ $filterInput['widget'] }}
             </div>
         @endforeach
-        {{ Form::submit('Filter', array('class' => 'btn btn-default')) }}
+        <div class="form-group">
+            <div class="btn-group">
+                {{ Form::submit('Filter', array('class' => 'btn btn-primary')) }}
+                <a href="{{ Request::url() }}" class="btn btn-default">Reset</a>
+            </div>
+        </div>
     {{ Form::close() }}
 </div>
